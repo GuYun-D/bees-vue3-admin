7683b4a11e03f616caf27f9b5b921874852f579a
solitary-cloud <1946306647@qq.com>
2022-11-03 13:25:16 +0800

    feat(permission): 添加权限验证

---
+++ src/permission.js
@@ -0,0 +1,27 @@
+import router from './router'
+import store from './store'
+
+// 路由白名单
+const whiteList = ['/login']
+
+/**
+ * 权限判断
+ * 路由前置守卫
+ */
+router.beforeEach((to, from, next) => {
+  if (store.getters.token) {
+    // 用户已登录不许进入 login
+    if (to.path === '/login') {
+      next('/')
+    } else {
+      next()
+    }
+  } else {
+    // 用户已登录，只允许进入login
+    if (whiteList.indexOf(to.path) > -1) {
+      next()
+    } else {
+      next('/login')
+    }
+  }
+})
