ca0e08610dfa6b7fffc4bdfce93690204bdf5d59
solitary-cloud <1946306647@qq.com>
2022-11-11 22:43:24 +0800

    feat(tagsview): 完成tags导航的右键菜单功能

--- src/components/TagsView/index.vue
+++ src/components/TagsView/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="tags-view-container">
     <router-link
+      @contextmenu.prevent="handleOpenMenu($event, index)"
       class="tags-view-item"
       :class="isActive(tag) ? 'active' : ''"
       :style="{
@@ -12,17 +13,26 @@
       :to="{ path: tag.fullPath }"
     >
       {{ tag.title }}
-      <svg-icon
-        v-show="!isActive(tag)"
-        icon="close"
-        @click.prevent.stop="handleClosePage(index)"
-      ></svg-icon>
+
+      <div class="close-wrapper" @click.prevent.stop="handleClosePage(index)">
+        <svg-icon v-show="!isActive(tag)" icon="close"></svg-icon>
+      </div>
     </router-link>
+
+    <ContaxtMenu
+      :index="selectIndex"
+      :style="menuStyle"
+      v-show="visiable"
+    ></ContaxtMenu>
   </div>
 </template>
 
 <script setup>
+import { ref, watch } from 'vue'
 import { useRoute } from 'vue-router'
+import { useStore } from 'vuex'
+import ContaxtMenu from './ContextMenu.vue'
+
 const route = useRoute()
 
 /**
@@ -32,7 +42,48 @@ const isActive = (tag) => {
   return tag.path === route.path
 }
 
-const handleClosePage = () => {}
+// 关闭tag
+const store = useStore()
+const handleClosePage = (index) => {
+  store.commit('app/removeTagsView', {
+    type: 'index',
+    index: selectIndex.value
+  })
+}
+
+/**
+ * 右键菜单
+ */
+const visiable = ref(false)
+const selectIndex = ref(0)
+const menuStyle = ref({
+  left: 0,
+  top: 0
+})
+const handleOpenMenu = (e, index) => {
+  const { x, y } = e
+  menuStyle.value.left = x + 'px'
+  menuStyle.value.top = y + 'px'
+  selectIndex.value = index
+
+  visiable.value = true
+}
+
+// menu 的关闭
+const closeMenus = () => {
+  visiable.value = false
+}
+
+watch(
+  () => visiable.value,
+  (val) => {
+    if (val) {
+      document.body.addEventListener('click', closeMenus)
+    } else {
+      document.body.removeEventListener('click', closeMenus)
+    }
+  }
+)
 </script>
 
 <style lang="scss" scoped>
@@ -43,60 +94,65 @@ const handleClosePage = () => {}
   border-bottom: 1px solid #d8dce5;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.12), 0 0 3px 0 rgba(0, 0, 0, 0.04);
   /* .tags-view-wrapper { */
-    .tags-view-item {
+  .tags-view-item {
+    display: inline-block;
+    position: relative;
+    cursor: pointer;
+    height: 26px;
+    line-height: 26px;
+    border: 1px solid #d8dce5;
+    color: #495060;
+    background: #fff;
+    padding: 0 8px;
+    font-size: 12px;
+    margin-left: 5px;
+    margin-top: 4px;
+    transition: all 200ms;
+
+    .close-wrapper {
       display: inline-block;
-      position: relative;
-      cursor: pointer;
-      height: 26px;
-      line-height: 26px;
-      border: 1px solid #d8dce5;
-      color: #495060;
-      background: #fff;
-      padding: 0 8px;
-      font-size: 12px;
-      margin-left: 5px;
-      margin-top: 4px;
-      transition: all 200ms;
-      &:first-of-type {
-        margin-left: 15px;
-      }
-      &:last-of-type {
-        margin-right: 15px;
-      }
-      &.active {
-        color: #fff;
-        &::before {
-          content: '';
-          background: #fff;
-          display: inline-block;
-          width: 8px;
-          height: 8px;
-          border-radius: 50%;
-          position: relative;
-          margin-right: 4px;
-        }
-      }
-      // close 按钮
-      .el-icon-close {
-        width: 16px;
-        height: 16px;
-        line-height: 10px;
-        vertical-align: 2px;
+    }
+
+    &:first-of-type {
+      margin-left: 15px;
+    }
+    &:last-of-type {
+      margin-right: 15px;
+    }
+    &.active {
+      color: #fff;
+      &::before {
+        content: '';
+        background: #fff;
+        display: inline-block;
+        width: 8px;
+        height: 8px;
         border-radius: 50%;
-        text-align: center;
-        transition: all 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
-        transform-origin: 100% 50%;
-        &:before {
-          transform: scale(0.6);
-          display: inline-block;
-          vertical-align: -3px;
-        }
-        &:hover {
-          background-color: #b4bccc;
-          color: #fff;
-        }
+        position: relative;
+        margin-right: 4px;
       }
     }
+    // close 按钮
+    .el-icon-close {
+      width: 16px;
+      height: 16px;
+      line-height: 10px;
+      vertical-align: 2px;
+      border-radius: 50%;
+      text-align: center;
+      transition: all 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
+      transform-origin: 100% 50%;
+      &:before {
+        transform: scale(0.6);
+        display: inline-block;
+        vertical-align: -3px;
+      }
+      &:hover {
+        background-color: #b4bccc;
+        color: #fff;
+      }
+    }
+  }
   /* } */
 }
 </style>
--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -2,7 +2,8 @@ import {
   TRIGGER_SIDEBAR_OPENED,
   SET_LANGUAGE,
   ADD_TAGS_VIEW_LIST,
-  CHANGE_TAGS_VIEW
+  CHANGE_TAGS_VIEW,
+  REMOVE_TAGS_VIEW
 } from './types'
 import { getItem, setItem } from '../../utils/storage'
 import { LANG, TAGS_VIEW } from '../../constant'
@@ -39,6 +40,35 @@ export default {
     // tagsView 国际化
     [CHANGE_TAGS_VIEW] (state, { index, tag }) {
       state.tagsViewList[index] = tag
+      setItem(TAGS_VIEW, state.tagsViewList)
+    },
+    /**
+     * 关闭tags
+     * @param {*} payload { other: 关闭其他, right: 关闭右侧, index: 删除当前 }
+     */
+    [REMOVE_TAGS_VIEW] (state, payload) {
+      switch (payload.type) {
+        case 'other':
+          state.tagsViewList.splice(
+            payload.index + 1,
+            state.tagsViewList.length - payload.index + 1
+          )
+
+          state.tagsViewList.splice(0, payload.index)
+          break
+
+        case 'right':
+          state.tagsViewList.splice(
+            payload.index + 1,
+            state.tagsViewList.length - payload.index + 1
+          )
+          break
+
+        case 'index':
+          state.tagsViewList.splice(payload.index, 1)
+          break
+      }
+
       setItem(TAGS_VIEW, state.tagsViewList)
     }
   }
